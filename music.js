298dd4deed0e240922c6cb6a983996f995031c22
basirat10 <basiratamusat10@gmail.com>
2024-04-01 18:08:16 +0100

    first commit

---
+++ music.js
@@ -0,0 +1,19 @@
+let songs = [
+    {
+ name: 'song 1',
+ path: 'music-1.mp4',
+ artist: 'music-1',
+ cover: 'image/music1.jpg',
+ 
+    },
+   
+   
+   
+    {
+        name: 'song 2',
+        path: 'music-2.mp4',
+        artist: 'music-2',
+        cover: 'image/music5.png'
+    }  
+]
+// export default songs
